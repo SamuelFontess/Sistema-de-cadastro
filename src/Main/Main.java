cd96a22cc3b528ffb62b0278deac147cd97ecf11
SamuelFontess <161073304+SamuelFontess@users.noreply.github.com>
2025-04-09 19:23:03 -0300

    passo 1

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,10 @@
-package Main;
+package src.main;
+
+import Repository.File;
 
 public class Main {
+    public static void main(String[] args) {
+        File file = new File();
+        file.createFile();
+    }
 }
